5cc439c4fdeedab07be253f82d6963b58b4e45dc
Ileolami <99335756+Ileolami@users.noreply.github.com>
2024-05-31 16:58:20 +0100

    Update ConfirmEmail.jsx

--- src/Pages/ConfirmEmail.jsx
+++ src/Pages/ConfirmEmail.jsx
@@ -9,7 +9,7 @@ const ConfirmEmail = () => {
         <AiOutlineMail className='mx-auto mb-2 text-6xl' />
         <h1 className=' mt-2 text-lg  mb-2 font-bold lett tracking-wider'>Confirm your email address</h1>
         <p className='mb-2'>We sent a confirmation email to</p>
-        <p className='font-bold mb-2'>email@gmail.com</p>
+        <p className='font-bold mb-2'><span></span>email@gmail.com</p>
         <p className='mb-2'>Check your email and click on <br /> the Confirmation link to continue</p>
         <button className='font-bold mt-32 w-32 h-10  rounded-lg  hover:bg-opacity-20 hover:border-[1.5px]' >Resend email</button>
       </div>
